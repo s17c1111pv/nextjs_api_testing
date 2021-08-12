5f289a70e0dcdca2789182d5c0b3653480d58ba3
s17c1111pv <88127845+s17c1111pv@users.noreply.github.com>
2021-08-12 14:40:22 +0900

    ALLOWED_HOSTS changed

--- backend/blog_prj/blog/settings.py
+++ backend/blog_prj/blog/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-# ALLOWED_HOSTS = ["blog-prj","localhost","127.0.0.1","nextjs-api-with-test.herokuapp.com"]
-ALLOWED_HOSTS=["*"]
+ALLOWED_HOSTS = ["blog-prj","localhost","127.0.0.1","nextjs-api-with-test.herokuapp.com"]
+# ALLOWED_HOSTS=["*"]
 
 # Application definition
 
