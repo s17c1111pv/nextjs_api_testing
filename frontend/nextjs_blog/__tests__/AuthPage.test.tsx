282ad6d09cc455c71252059b2a1aa09cc79fe127
s17c1111pv <88127845+s17c1111pv@users.noreply.github.com>
2021-08-12 11:25:01 +0900

    first commit

---
+++ frontend/nextjs_blog/__tests__/AuthPage.test.tsx
@@ -0,0 +1,152 @@
+import { render, screen, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { getPage } from 'next-page-tester'
+import { initTestHelpers } from 'next-page-tester'
+
+initTestHelpers()
+
+process.env.NEXT_PUBLIC_RESTAPI_URL = 'http://blog-prj:8000/api'
+process.env.NEXT_PUBLIC_RESTAPI_URLB = 'http://127.0.0.1:8000/api'
+
+const handlers = [
+  rest.post(
+    `${process.env.NEXT_PUBLIC_RESTAPI_URLB}/jwt/create/`,
+    (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json({ access: '123xyz' }))
+    }
+  ),
+  rest.post(
+    `${process.env.NEXT_PUBLIC_RESTAPI_URLB}/register/`,
+    (req, res, ctx) => {
+      return res(ctx.status(201))
+    }
+  ),
+  rest.get(
+    `${process.env.NEXT_PUBLIC_RESTAPI_URL}/get-blogs/`,
+    (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json([
+          {
+            id: 1,
+            title: 'title1',
+            content: 'content1',
+            username: 'username1',
+            tags: [
+              { id: 1, name: 'tag1' },
+              { id: 2, name: 'tag2' },
+            ],
+            created_at: '2021-01-12 14:59:41',
+          },
+          {
+            id: 2,
+            title: 'title2',
+            content: 'content2',
+            username: 'username2',
+            tags: [
+              { id: 1, name: 'tag1' },
+              { id: 2, name: 'tag2' },
+            ],
+            created_at: '2021-01-13 14:59:41',
+          },
+        ])
+      )
+    }
+  ),
+]
+const server = setupServer(...handlers)
+beforeAll(() => {
+  server.listen()
+})
+afterEach(() => {
+  server.resetHandlers()
+  cleanup()
+})
+afterAll(() => {
+  server.close()
+})
+
+describe('AdminPage test cases', () => {
+  it('Should route to index-page when login succeeded', async () => {
+    const { page } = await getPage({
+      route: '/admin-page',
+    })
+    render(page)
+    expect(await screen.findByText('Login')).toBeInTheDocument()
+    userEvent.type(screen.getByPlaceholderText('Username'), 'user1')
+    userEvent.type(screen.getByPlaceholderText('Password'), 'dummypw')
+    userEvent.click(screen.getByText('Login with JWT'))
+    expect(await screen.findByText('blog page')).toBeInTheDocument()
+  })
+  it('Should not route to index-page when login is failed', async () => {
+    server.use(
+      rest.post(
+        `${process.env.NEXT_PUBLIC_RESTAPI_URLB}/jwt/create/`,
+        (req, res, ctx) => {
+          return res(ctx.status(400))
+        }
+      )
+    )
+    const { page } = await getPage({
+      route: '/admin-page',
+    })
+    render(page)
+    expect(await screen.findByText('Login')).toBeInTheDocument()
+    userEvent.type(screen.getByPlaceholderText('Username'), 'user1')
+    userEvent.type(screen.getByPlaceholderText('Password'), 'dummypw')
+    userEvent.click(screen.getByText('Login with JWT'))
+    expect(await screen.findByText('Login Error')).toBeInTheDocument()
+    expect(screen.getByText('Login')).toBeInTheDocument()
+    expect(screen.queryByText('blog page')).toBeNull()
+  })
+  it('Should change to register mode', async () => {
+    const { page } = await getPage({
+      route: '/admin-page',
+    })
+    render(page)
+    expect(await screen.findByText('Login')).toBeInTheDocument()
+    expect(screen.getByText('Login with JWT')).toBeInTheDocument()
+    userEvent.click(screen.getByTestId('mode-change'))
+    expect(await screen.findByText('Sign up')).toBeInTheDocument()
+    expect(screen.getByText('Create new user')).toBeInTheDocument()
+  })
+  it('Should route to index-page when register+login succeed', async () => {
+    const { page } = await getPage({
+      route: '/admin-page',
+    })
+    render(page)
+    expect(await screen.findByText('Login')).toBeInTheDocument()
+    userEvent.click(screen.getByTestId('mode-change'))
+    expect(await screen.findByText('Sign up')).toBeInTheDocument()
+    userEvent.type(screen.getByPlaceholderText('Username'), 'user1')
+    userEvent.type(screen.getByPlaceholderText('Password'), 'dummypw')
+    userEvent.click(screen.getByText('Create new user'))
+    expect(await screen.findByText('blog page')).toBeInTheDocument()
+  })
+  it('Should not route to index-page when register failed', async () => {
+    server.use(
+      rest.post(
+        `${process.env.NEXT_PUBLIC_RESTAPI_URLB}/register/`,
+        (req, res, ctx) => {
+          return res(ctx.status(400))
+        }
+      )
+    )
+    const { page } = await getPage({
+      route: '/admin-page',
+    })
+    render(page)
+    expect(await screen.findByText('Login')).toBeInTheDocument()
+    userEvent.click(screen.getByTestId('mode-change'))
+    expect(await screen.findByText('Sign up')).toBeInTheDocument()
+    userEvent.type(screen.getByPlaceholderText('Username'), 'user1')
+    userEvent.type(screen.getByPlaceholderText('Password'), 'dummypw')
+    userEvent.click(screen.getByText('Create new user'))
+    expect(await screen.findByText('Registration Error')).toBeInTheDocument()
+    expect(screen.getByText('Sign up')).toBeInTheDocument()
+    expect(screen.queryByText('blog page')).toBeNull()
+  })
+})
---
+++ frontend/nextjs_blog/pages/admin-page.tsx
@@ -0,0 +1,12 @@
+import Auth from '../components/Auth'
+import Layout from '../components/Layout'
+
+const AdminPage: React.FC = () => {
+  return (
+    <Layout title="admin">
+      <Auth />
+    </Layout>
+  )
+}
+
+export default AdminPage
